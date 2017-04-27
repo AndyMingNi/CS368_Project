00290f8229248181003eeb51264be93ad2a57138
x6tr2ni <x6tr2ni@yahoo.com>
2017-04-27 00:59:45 -0500

    Renamed objects

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -34,23 +34,39 @@ namespace Project1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::MonthCalendar^  monthCalendar1;
+	private: System::Windows::Forms::MonthCalendar^  monthCalendar;
+	protected:
+
 	protected:
 	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::DateTimePicker^  dateTimePicker1;
+	private: System::Windows::Forms::Label^  labelNotes;
+
+	private: System::Windows::Forms::Label^  labelEventTitle;
+
+	private: System::Windows::Forms::Label^  labelDayPicker;
+
+	private: System::Windows::Forms::TextBox^  eventTitleTF;
+	private: System::Windows::Forms::DateTimePicker^  dateTimePicker;
+
+
 	private: System::Windows::Forms::Timer^  timer1;
-	private: System::Windows::Forms::Label^  label4;
-	private: System::Windows::Forms::TextBox^  textBox2;
-	private: System::Windows::Forms::NumericUpDown^  numericUpDown1;
-	private: System::Windows::Forms::Label^  label5;
-	private: System::Windows::Forms::ListView^  listView1;
-	private: System::Windows::Forms::Button^  button2;
-	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::CheckBox^  checkBox1;
+	private: System::Windows::Forms::Label^  labelAlarmPicker;
+
+	private: System::Windows::Forms::TextBox^  NotesTF;
+	private: System::Windows::Forms::NumericUpDown^  alarmTimePicker;
+	private: System::Windows::Forms::Label^  labelUEO;
+
+
+
+
+	private: System::Windows::Forms::ListView^  eventPreview;
+	private: System::Windows::Forms::Button^  buttonNewClear;
+
+
+	private: System::Windows::Forms::Button^  buttonSaveEvent;
+
+	private: System::Windows::Forms::CheckBox^  alarmCheckBox;
+
 
 	private: System::ComponentModel::IContainer^  components;
 
@@ -70,45 +86,45 @@ namespace Project1 {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
-			this->monthCalendar1 = (gcnew System::Windows::Forms::MonthCalendar());
+			this->monthCalendar = (gcnew System::Windows::Forms::MonthCalendar());
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->buttonNewClear = (gcnew System::Windows::Forms::Button());
+			this->buttonSaveEvent = (gcnew System::Windows::Forms::Button());
+			this->alarmCheckBox = (gcnew System::Windows::Forms::CheckBox());
+			this->NotesTF = (gcnew System::Windows::Forms::TextBox());
+			this->alarmTimePicker = (gcnew System::Windows::Forms::NumericUpDown());
+			this->labelAlarmPicker = (gcnew System::Windows::Forms::Label());
+			this->eventTitleTF = (gcnew System::Windows::Forms::TextBox());
+			this->dateTimePicker = (gcnew System::Windows::Forms::DateTimePicker());
+			this->labelNotes = (gcnew System::Windows::Forms::Label());
+			this->labelEventTitle = (gcnew System::Windows::Forms::Label());
+			this->labelDayPicker = (gcnew System::Windows::Forms::Label());
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->listView1 = (gcnew System::Windows::Forms::ListView());
-			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->labelUEO = (gcnew System::Windows::Forms::Label());
+			this->eventPreview = (gcnew System::Windows::Forms::ListView());
 			this->groupBox1->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->alarmTimePicker))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// monthCalendar1
+			// monthCalendar
 			// 
-			this->monthCalendar1->Location = System::Drawing::Point(18, 18);
-			this->monthCalendar1->Name = L"monthCalendar1";
-			this->monthCalendar1->TabIndex = 0;
+			this->monthCalendar->Location = System::Drawing::Point(18, 18);
+			this->monthCalendar->Name = L"monthCalendar";
+			this->monthCalendar->TabIndex = 0;
 			// 
 			// groupBox1
 			// 
-			this->groupBox1->Controls->Add(this->button2);
-			this->groupBox1->Controls->Add(this->button1);
-			this->groupBox1->Controls->Add(this->checkBox1);
-			this->groupBox1->Controls->Add(this->textBox2);
-			this->groupBox1->Controls->Add(this->numericUpDown1);
-			this->groupBox1->Controls->Add(this->label4);
-			this->groupBox1->Controls->Add(this->textBox1);
-			this->groupBox1->Controls->Add(this->dateTimePicker1);
-			this->groupBox1->Controls->Add(this->label3);
-			this->groupBox1->Controls->Add(this->label2);
-			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->buttonNewClear);
+			this->groupBox1->Controls->Add(this->buttonSaveEvent);
+			this->groupBox1->Controls->Add(this->alarmCheckBox);
+			this->groupBox1->Controls->Add(this->NotesTF);
+			this->groupBox1->Controls->Add(this->alarmTimePicker);
+			this->groupBox1->Controls->Add(this->labelAlarmPicker);
+			this->groupBox1->Controls->Add(this->eventTitleTF);
+			this->groupBox1->Controls->Add(this->dateTimePicker);
+			this->groupBox1->Controls->Add(this->labelNotes);
+			this->groupBox1->Controls->Add(this->labelEventTitle);
+			this->groupBox1->Controls->Add(this->labelDayPicker);
 			this->groupBox1->Location = System::Drawing::Point(257, 18);
 			this->groupBox1->Name = L"groupBox1";
 			this->groupBox1->Size = System::Drawing::Size(415, 328);
@@ -117,138 +133,138 @@ namespace Project1 {
 			this->groupBox1->Text = L"New Event";
 			this->groupBox1->Enter += gcnew System::EventHandler(this, &MyForm::groupBox1_Enter);
 			// 
-			// label1
+			// buttonNewClear
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(7, 31);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(62, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Day Picker:";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->buttonNewClear->Location = System::Drawing::Point(328, 294);
+			this->buttonNewClear->Name = L"buttonNewClear";
+			this->buttonNewClear->Size = System::Drawing::Size(75, 23);
+			this->buttonNewClear->TabIndex = 13;
+			this->buttonNewClear->Text = L"New/Clear";
+			this->buttonNewClear->UseVisualStyleBackColor = true;
 			// 
-			// label2
+			// buttonSaveEvent
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(7, 81);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(61, 13);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"Event Title:";
-			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			this->buttonSaveEvent->Location = System::Drawing::Point(52, 294);
+			this->buttonSaveEvent->Name = L"buttonSaveEvent";
+			this->buttonSaveEvent->Size = System::Drawing::Size(75, 23);
+			this->buttonSaveEvent->TabIndex = 12;
+			this->buttonSaveEvent->Text = L"Save Event";
+			this->buttonSaveEvent->UseVisualStyleBackColor = true;
 			// 
-			// label3
+			// alarmCheckBox
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(7, 132);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(38, 13);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"Notes:";
-			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
+			this->alarmCheckBox->AutoSize = true;
+			this->alarmCheckBox->CheckAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->alarmCheckBox->Location = System::Drawing::Point(266, 50);
+			this->alarmCheckBox->Name = L"alarmCheckBox";
+			this->alarmCheckBox->Size = System::Drawing::Size(97, 17);
+			this->alarmCheckBox->TabIndex = 10;
+			this->alarmCheckBox->Text = L"Alarm (On/Off):";
+			this->alarmCheckBox->UseVisualStyleBackColor = true;
 			// 
-			// dateTimePicker1
+			// NotesTF
 			// 
-			this->dateTimePicker1->Location = System::Drawing::Point(52, 47);
-			this->dateTimePicker1->Name = L"dateTimePicker1";
-			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
-			this->dateTimePicker1->TabIndex = 3;
+			this->NotesTF->Location = System::Drawing::Point(52, 153);
+			this->NotesTF->Multiline = true;
+			this->NotesTF->Name = L"NotesTF";
+			this->NotesTF->Size = System::Drawing::Size(351, 118);
+			this->NotesTF->TabIndex = 8;
 			// 
-			// textBox1
+			// alarmTimePicker
 			// 
-			this->textBox1->Location = System::Drawing::Point(52, 98);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(200, 20);
-			this->textBox1->TabIndex = 4;
+			this->alarmTimePicker->Location = System::Drawing::Point(311, 97);
+			this->alarmTimePicker->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 48, 0, 0, 0 });
+			this->alarmTimePicker->Name = L"alarmTimePicker";
+			this->alarmTimePicker->Size = System::Drawing::Size(92, 20);
+			this->alarmTimePicker->TabIndex = 7;
+			this->alarmTimePicker->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown1_ValueChanged);
 			// 
-			// label4
+			// labelAlarmPicker
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(266, 81);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(97, 13);
-			this->label4->TabIndex = 6;
-			this->label4->Text = L"Alarm (0-48 Hours):";
-			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
+			this->labelAlarmPicker->AutoSize = true;
+			this->labelAlarmPicker->Location = System::Drawing::Point(266, 81);
+			this->labelAlarmPicker->Name = L"labelAlarmPicker";
+			this->labelAlarmPicker->Size = System::Drawing::Size(97, 13);
+			this->labelAlarmPicker->TabIndex = 6;
+			this->labelAlarmPicker->Text = L"Alarm (0-48 Hours):";
+			this->labelAlarmPicker->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
 			// 
-			// numericUpDown1
+			// eventTitleTF
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(311, 97);
-			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 48, 0, 0, 0 });
-			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(92, 20);
-			this->numericUpDown1->TabIndex = 7;
-			this->numericUpDown1->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown1_ValueChanged);
+			this->eventTitleTF->Location = System::Drawing::Point(52, 98);
+			this->eventTitleTF->Name = L"eventTitleTF";
+			this->eventTitleTF->Size = System::Drawing::Size(200, 20);
+			this->eventTitleTF->TabIndex = 4;
 			// 
-			// textBox2
+			// dateTimePicker
 			// 
-			this->textBox2->Location = System::Drawing::Point(52, 153);
-			this->textBox2->Multiline = true;
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(351, 118);
-			this->textBox2->TabIndex = 8;
+			this->dateTimePicker->Location = System::Drawing::Point(52, 47);
+			this->dateTimePicker->Name = L"dateTimePicker";
+			this->dateTimePicker->Size = System::Drawing::Size(200, 20);
+			this->dateTimePicker->TabIndex = 3;
 			// 
-			// label5
+			// labelNotes
 			// 
-			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(18, 193);
-			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(142, 13);
-			this->label5->TabIndex = 2;
-			this->label5->Text = L"Upcoming Events Overview:";
+			this->labelNotes->AutoSize = true;
+			this->labelNotes->Location = System::Drawing::Point(7, 132);
+			this->labelNotes->Name = L"labelNotes";
+			this->labelNotes->Size = System::Drawing::Size(38, 13);
+			this->labelNotes->TabIndex = 2;
+			this->labelNotes->Text = L"Notes:";
+			this->labelNotes->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
 			// 
-			// listView1
+			// labelEventTitle
 			// 
-			this->listView1->GridLines = true;
-			this->listView1->Location = System::Drawing::Point(21, 209);
-			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(224, 137);
-			this->listView1->TabIndex = 3;
-			this->listView1->UseCompatibleStateImageBehavior = false;
+			this->labelEventTitle->AutoSize = true;
+			this->labelEventTitle->Location = System::Drawing::Point(7, 81);
+			this->labelEventTitle->Name = L"labelEventTitle";
+			this->labelEventTitle->Size = System::Drawing::Size(61, 13);
+			this->labelEventTitle->TabIndex = 1;
+			this->labelEventTitle->Text = L"Event Title:";
+			this->labelEventTitle->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
-			// checkBox1
+			// labelDayPicker
 			// 
-			this->checkBox1->AutoSize = true;
-			this->checkBox1->CheckAlign = System::Drawing::ContentAlignment::MiddleRight;
-			this->checkBox1->Location = System::Drawing::Point(266, 50);
-			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(97, 17);
-			this->checkBox1->TabIndex = 10;
-			this->checkBox1->Text = L"Alarm (On/Off):";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->labelDayPicker->AutoSize = true;
+			this->labelDayPicker->Location = System::Drawing::Point(7, 31);
+			this->labelDayPicker->Name = L"labelDayPicker";
+			this->labelDayPicker->Size = System::Drawing::Size(62, 13);
+			this->labelDayPicker->TabIndex = 0;
+			this->labelDayPicker->Text = L"Day Picker:";
+			this->labelDayPicker->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
-			// button1
+			// labelUEO
 			// 
-			this->button1->Location = System::Drawing::Point(52, 294);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 12;
-			this->button1->Text = L"Save Event";
-			this->button1->UseVisualStyleBackColor = true;
+			this->labelUEO->AutoSize = true;
+			this->labelUEO->Location = System::Drawing::Point(18, 193);
+			this->labelUEO->Name = L"labelUEO";
+			this->labelUEO->Size = System::Drawing::Size(142, 13);
+			this->labelUEO->TabIndex = 2;
+			this->labelUEO->Text = L"Upcoming Events Overview:";
 			// 
-			// button2
+			// eventPreview
 			// 
-			this->button2->Location = System::Drawing::Point(328, 294);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
-			this->button2->TabIndex = 13;
-			this->button2->Text = L"New/Clear";
-			this->button2->UseVisualStyleBackColor = true;
+			this->eventPreview->GridLines = true;
+			this->eventPreview->Location = System::Drawing::Point(21, 209);
+			this->eventPreview->Name = L"eventPreview";
+			this->eventPreview->Size = System::Drawing::Size(224, 137);
+			this->eventPreview->TabIndex = 3;
+			this->eventPreview->UseCompatibleStateImageBehavior = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(691, 355);
-			this->Controls->Add(this->listView1);
-			this->Controls->Add(this->label5);
+			this->Controls->Add(this->eventPreview);
+			this->Controls->Add(this->labelUEO);
 			this->Controls->Add(this->groupBox1);
-			this->Controls->Add(this->monthCalendar1);
+			this->Controls->Add(this->monthCalendar);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Personal Event Calendar";
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->alarmTimePicker))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
