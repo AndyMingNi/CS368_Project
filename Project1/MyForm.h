2b19c64a47dfc0722aca9b73ac8507e3a49a6bb2
T-martens <tbmartens@wisc.edu>
2017-05-02 17:00:43 -0500

    List is now Vertical
    It has text all on one row.

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -20,6 +20,8 @@ namespace Project1 {
 		String ^ userNotes;
 		String ^ lineData;
 		StreamWriter ^ outputStream;
+	private: System::Windows::Forms::ColumnHeader^  columnHeader1;
+	public:
 		StreamReader ^ inputStream;
 
 		MyForm(void)
@@ -102,13 +104,15 @@ namespace Project1 {
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->labelUEO = (gcnew System::Windows::Forms::Label());
 			this->eventPreview = (gcnew System::Windows::Forms::ListView());
+			this->columnHeader1 = (gcnew System::Windows::Forms::ColumnHeader());
 			this->groupBox1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->alarmTimePicker))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// monthCalendar
 			// 
-			this->monthCalendar->Location = System::Drawing::Point(18, 18);
+			this->monthCalendar->Location = System::Drawing::Point(24, 22);
+			this->monthCalendar->Margin = System::Windows::Forms::Padding(12, 11, 12, 11);
 			this->monthCalendar->Name = L"monthCalendar";
 			this->monthCalendar->TabIndex = 0;
 			// 
@@ -125,18 +129,21 @@ namespace Project1 {
 			this->groupBox1->Controls->Add(this->labelNotes);
 			this->groupBox1->Controls->Add(this->labelEventTitle);
 			this->groupBox1->Controls->Add(this->labelDatePicker);
-			this->groupBox1->Location = System::Drawing::Point(257, 18);
+			this->groupBox1->Location = System::Drawing::Point(343, 22);
+			this->groupBox1->Margin = System::Windows::Forms::Padding(4);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(415, 328);
+			this->groupBox1->Padding = System::Windows::Forms::Padding(4);
+			this->groupBox1->Size = System::Drawing::Size(553, 404);
 			this->groupBox1->TabIndex = 1;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"New Event";
 			// 
 			// buttonNewClear
 			// 
-			this->buttonNewClear->Location = System::Drawing::Point(328, 294);
+			this->buttonNewClear->Location = System::Drawing::Point(437, 362);
+			this->buttonNewClear->Margin = System::Windows::Forms::Padding(4);
 			this->buttonNewClear->Name = L"buttonNewClear";
-			this->buttonNewClear->Size = System::Drawing::Size(75, 23);
+			this->buttonNewClear->Size = System::Drawing::Size(100, 28);
 			this->buttonNewClear->TabIndex = 13;
 			this->buttonNewClear->Text = L"New/Clear";
 			this->buttonNewClear->UseVisualStyleBackColor = true;
@@ -144,9 +151,10 @@ namespace Project1 {
 			// 
 			// buttonSaveEvent
 			// 
-			this->buttonSaveEvent->Location = System::Drawing::Point(52, 294);
+			this->buttonSaveEvent->Location = System::Drawing::Point(69, 362);
+			this->buttonSaveEvent->Margin = System::Windows::Forms::Padding(4);
 			this->buttonSaveEvent->Name = L"buttonSaveEvent";
-			this->buttonSaveEvent->Size = System::Drawing::Size(75, 23);
+			this->buttonSaveEvent->Size = System::Drawing::Size(100, 28);
 			this->buttonSaveEvent->TabIndex = 12;
 			this->buttonSaveEvent->Text = L"Save Event";
 			this->buttonSaveEvent->UseVisualStyleBackColor = true;
@@ -156,44 +164,49 @@ namespace Project1 {
 			// 
 			this->alarmCheckBox->AutoSize = true;
 			this->alarmCheckBox->CheckAlign = System::Drawing::ContentAlignment::MiddleRight;
-			this->alarmCheckBox->Location = System::Drawing::Point(266, 50);
+			this->alarmCheckBox->Location = System::Drawing::Point(355, 62);
+			this->alarmCheckBox->Margin = System::Windows::Forms::Padding(4);
 			this->alarmCheckBox->Name = L"alarmCheckBox";
-			this->alarmCheckBox->Size = System::Drawing::Size(97, 17);
+			this->alarmCheckBox->Size = System::Drawing::Size(126, 21);
 			this->alarmCheckBox->TabIndex = 10;
 			this->alarmCheckBox->Text = L"Alarm (On/Off):";
 			this->alarmCheckBox->UseVisualStyleBackColor = true;
 			// 
 			// notesTF
 			// 
-			this->notesTF->Location = System::Drawing::Point(52, 153);
+			this->notesTF->Location = System::Drawing::Point(69, 188);
+			this->notesTF->Margin = System::Windows::Forms::Padding(4);
 			this->notesTF->Multiline = true;
 			this->notesTF->Name = L"notesTF";
-			this->notesTF->Size = System::Drawing::Size(350, 118);
+			this->notesTF->Size = System::Drawing::Size(465, 144);
 			this->notesTF->TabIndex = 8;
 			this->notesTF->TextChanged += gcnew System::EventHandler(this, &MyForm::notesTF_TextChanged);
 			// 
 			// alarmTimePicker
 			// 
-			this->alarmTimePicker->Location = System::Drawing::Point(311, 97);
+			this->alarmTimePicker->Location = System::Drawing::Point(415, 119);
+			this->alarmTimePicker->Margin = System::Windows::Forms::Padding(4);
 			this->alarmTimePicker->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 48, 0, 0, 0 });
 			this->alarmTimePicker->Name = L"alarmTimePicker";
-			this->alarmTimePicker->Size = System::Drawing::Size(92, 20);
+			this->alarmTimePicker->Size = System::Drawing::Size(123, 22);
 			this->alarmTimePicker->TabIndex = 7;
 			// 
 			// labelAlarmPicker
 			// 
 			this->labelAlarmPicker->AutoSize = true;
-			this->labelAlarmPicker->Location = System::Drawing::Point(266, 81);
+			this->labelAlarmPicker->Location = System::Drawing::Point(355, 100);
+			this->labelAlarmPicker->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelAlarmPicker->Name = L"labelAlarmPicker";
-			this->labelAlarmPicker->Size = System::Drawing::Size(97, 13);
+			this->labelAlarmPicker->Size = System::Drawing::Size(133, 17);
 			this->labelAlarmPicker->TabIndex = 6;
 			this->labelAlarmPicker->Text = L"Alarm (0-48 Hours):";
 			// 
 			// eventTitleTF
 			// 
-			this->eventTitleTF->Location = System::Drawing::Point(52, 98);
+			this->eventTitleTF->Location = System::Drawing::Point(69, 121);
+			this->eventTitleTF->Margin = System::Windows::Forms::Padding(4);
 			this->eventTitleTF->Name = L"eventTitleTF";
-			this->eventTitleTF->Size = System::Drawing::Size(200, 20);
+			this->eventTitleTF->Size = System::Drawing::Size(265, 22);
 			this->eventTitleTF->TabIndex = 4;
 			this->eventTitleTF->TextChanged += gcnew System::EventHandler(this, &MyForm::eventTitleTF_TextChanged);
 			// 
@@ -201,67 +214,80 @@ namespace Project1 {
 			// 
 			this->dateTimePicker->CustomFormat = L"MM/dd/yyyy hh:mm tt";
 			this->dateTimePicker->Format = System::Windows::Forms::DateTimePickerFormat::Custom;
-			this->dateTimePicker->Location = System::Drawing::Point(52, 47);
+			this->dateTimePicker->Location = System::Drawing::Point(69, 58);
+			this->dateTimePicker->Margin = System::Windows::Forms::Padding(4);
 			this->dateTimePicker->Name = L"dateTimePicker";
 			this->dateTimePicker->ShowUpDown = true;
-			this->dateTimePicker->Size = System::Drawing::Size(200, 20);
+			this->dateTimePicker->Size = System::Drawing::Size(265, 22);
 			this->dateTimePicker->TabIndex = 3;
 			this->dateTimePicker->ValueChanged += gcnew System::EventHandler(this, &MyForm::dateTimePicker_ValueChanged);
 			// 
 			// labelNotes
 			// 
 			this->labelNotes->AutoSize = true;
-			this->labelNotes->Location = System::Drawing::Point(7, 132);
+			this->labelNotes->Location = System::Drawing::Point(9, 162);
+			this->labelNotes->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelNotes->Name = L"labelNotes";
-			this->labelNotes->Size = System::Drawing::Size(38, 13);
+			this->labelNotes->Size = System::Drawing::Size(49, 17);
 			this->labelNotes->TabIndex = 2;
 			this->labelNotes->Text = L"Notes:";
 			// 
 			// labelEventTitle
 			// 
 			this->labelEventTitle->AutoSize = true;
-			this->labelEventTitle->Location = System::Drawing::Point(7, 81);
+			this->labelEventTitle->Location = System::Drawing::Point(9, 100);
+			this->labelEventTitle->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelEventTitle->Name = L"labelEventTitle";
-			this->labelEventTitle->Size = System::Drawing::Size(61, 13);
+			this->labelEventTitle->Size = System::Drawing::Size(79, 17);
 			this->labelEventTitle->TabIndex = 1;
 			this->labelEventTitle->Text = L"Event Title:";
 			// 
 			// labelDatePicker
 			// 
 			this->labelDatePicker->AutoSize = true;
-			this->labelDatePicker->Location = System::Drawing::Point(7, 31);
+			this->labelDatePicker->Location = System::Drawing::Point(9, 38);
+			this->labelDatePicker->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelDatePicker->Name = L"labelDatePicker";
-			this->labelDatePicker->Size = System::Drawing::Size(66, 13);
+			this->labelDatePicker->Size = System::Drawing::Size(85, 17);
 			this->labelDatePicker->TabIndex = 0;
 			this->labelDatePicker->Text = L"Date Picker:";
 			// 
 			// labelUEO
 			// 
 			this->labelUEO->AutoSize = true;
-			this->labelUEO->Location = System::Drawing::Point(18, 193);
+			this->labelUEO->Location = System::Drawing::Point(24, 238);
+			this->labelUEO->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelUEO->Name = L"labelUEO";
-			this->labelUEO->Size = System::Drawing::Size(142, 13);
+			this->labelUEO->Size = System::Drawing::Size(184, 17);
 			this->labelUEO->TabIndex = 2;
 			this->labelUEO->Text = L"Upcoming Events Overview:";
 			// 
 			// eventPreview
 			// 
+			this->eventPreview->Alignment = System::Windows::Forms::ListViewAlignment::Left;
+			this->eventPreview->View = View::Details;
+			this->eventPreview->AutoArrange = false;
+			this->eventPreview->FullRowSelect = true;
 			this->eventPreview->GridLines = true;
-			this->eventPreview->Location = System::Drawing::Point(21, 209);
+			this->eventPreview->LabelWrap = false;
+			this->eventPreview->Location = System::Drawing::Point(28, 257);
+			this->eventPreview->Margin = System::Windows::Forms::Padding(4);
+			this->eventPreview->Columns->Add("event", -2, HorizontalAlignment::Left);
 			this->eventPreview->Name = L"eventPreview";
-			this->eventPreview->Size = System::Drawing::Size(224, 137);
+			this->eventPreview->Size = System::Drawing::Size(297, 168);
 			this->eventPreview->TabIndex = 3;
 			this->eventPreview->UseCompatibleStateImageBehavior = false;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(691, 368);
+			this->ClientSize = System::Drawing::Size(921, 453);
 			this->Controls->Add(this->eventPreview);
 			this->Controls->Add(this->labelUEO);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->monthCalendar);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
 			this->Text = L"Personal Event Calendar";
 			this->groupBox1->ResumeLayout(false);
@@ -279,7 +305,9 @@ namespace Project1 {
 			this->eventPreview->Items->Clear();
 			while (lineData != nullptr) {
 				lineData = lineData->Replace("$@$", " ");
-				this->eventPreview->Items->Add(lineData);
+				lineData = lineData->Replace("  ", " ");
+				//this->eventPreview->Columns->TextAlign = HorizontalAlignment::Right;
+				this->eventPreview->Items->Add(gcnew ListViewItem(lineData	 ));
 
 				lineData = inputStream->ReadLine();
 			}
